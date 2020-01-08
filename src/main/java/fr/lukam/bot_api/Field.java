0bf63d3fb084f93f4794af1a8e2f561f1918aa93
LukaMrt <maret.luka@gmail.com>
2020-01-08 14:05:51 +0100

    ✨ add Field

---
+++ src/main/java/fr/lukam/bot_api/Field.java
@@ -0,0 +1,13 @@
+package fr.lukam.bot_api;
+
+public interface Field {
+
+    String getTitle();
+
+    void setTitle(String title);
+
+    String getContent();
+
+    void setContent(String content);
+
+}
