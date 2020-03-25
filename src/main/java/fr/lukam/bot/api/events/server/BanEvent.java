26eed9693fcbec7f5c0de5d6bb277d3e4592d1f6
Luka <nespote@gmail.com>
2020-03-25 16:14:33 +0100

    🚑 fix bug in BanEvent

--- src/main/java/fr/lukam/bot/api/events/server/BanEvent.java
+++ src/main/java/fr/lukam/bot/api/events/server/BanEvent.java
@@ -2,5 +2,5 @@ package fr.lukam.bot.api.events.server;
 
 import fr.lukam.bot.api.events.user.UserEvent;
 
-public interface BanEvent extends UserEvent {
+public interface BanEvent extends UserEvent, ServerEvent {
 }
