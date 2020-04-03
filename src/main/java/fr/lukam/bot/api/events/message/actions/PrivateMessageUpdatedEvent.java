38cd42e8fb5b958056e6a5f540cd377084905c01
LukaMrt <maret.luka@gmail.com>
2020-04-03 11:43:18 +0200

    🚨 remove codacy warnings

--- src/main/java/fr/lukam/bot/api/events/message/actions/PrivateMessageUpdatedEvent.java
+++ src/main/java/fr/lukam/bot/api/events/message/actions/PrivateMessageUpdatedEvent.java
@@ -1,6 +1,5 @@
 package fr.lukam.bot.api.events.message.actions;
 
-import fr.lukam.bot.api.entities.interfaces.message.Message;
 import fr.lukam.bot.api.events.message.MessageEvent;
 
 public interface PrivateMessageUpdatedEvent extends MessageEvent {
