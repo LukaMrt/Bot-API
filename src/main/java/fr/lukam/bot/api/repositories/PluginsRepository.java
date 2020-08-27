91682c7e5d58ee053539e44103622e75e984159a
LukaMrt <maret.luka@gmail.com>
2020-08-27 10:39:30 +0200

    ✨ add methods to register listeners and commands, load, start and stop plugins

--- src/main/java/fr/lukam/bot/api/repositories/PluginsRepository.java
+++ src/main/java/fr/lukam/bot/api/repositories/PluginsRepository.java
@@ -6,6 +6,16 @@ import java.util.List;
 
 public interface PluginsRepository {
 
+    void loadPlugins();
+
+    void startPlugins();
+
+    void stopPlugins();
+
+    void registerCommands(CommandsRepository commandsRepository);
+
+    void registerListeners(ListenersRepository listenersRepository);
+
     List<Plugin> getPlugins();
 
     Plugin getPluginByName(String name);
