ef7645515e1ac648a4af06ef01b645650e2085b4
LukaMrt <maret.luka@gmail.com>
2020-08-27 10:09:04 +0200

    ✨ add methods to register listeners and commands

--- src/main/java/fr/lukam/bot/api/repositories/CommandsRepository.java
+++ src/main/java/fr/lukam/bot/api/repositories/CommandsRepository.java
@@ -1,10 +1,13 @@
 package fr.lukam.bot.api.repositories;
 
 import fr.lukam.bot.api.entities.interfaces.commands.Command;
+import fr.lukam.bot.api.entities.interfaces.events.Listener;
 
 import java.util.List;
 
 public interface CommandsRepository {
+
+    void registerCommands(List<Command> commands);
 
     List<Command> getCommands();
 
--- src/main/java/fr/lukam/bot/api/repositories/ListenersRepository.java
+++ src/main/java/fr/lukam/bot/api/repositories/ListenersRepository.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface ListenersRepository {
 
+    void registerListener(List<Listener> listeners);
+
     List<Listener> getListeners();
 
     Listener getListenerByName(String name);
